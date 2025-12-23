024457bac10ed0b7679ca350e1ed2ab6d5490e68
Josue Essotom Danigue <josdanigue@gmail.com>
2025-12-23 03:32:42 +0000

    Update login.ts

--- tests/utils/login.ts
+++ tests/utils/login.ts
@@ -2,8 +2,8 @@ import{Page,  expect } from'@playwright/test'
 
 export async function login(page: Page) {
     await page.goto("https://test-01.tech11.com/");
-    await page.fill('[name="username"]', 'claudia.niemeier@example.com');
-    await page.fill('[name="password"]', 'ixiKRjGTws2zDYiHU5nfLYBz2fzSGCvK');
+    await page.fill('[name="username"]', 'loto@example.com');
+    await page.fill('[name="password"]', 'ixiKRjGTws2zDYiHU5nfLYBfzSGCvK');
     await page.click('#kc-login');
 
     await expect(page.locator('h1.display-1.m-5:has-text("tech11")')).toBeVisible();
